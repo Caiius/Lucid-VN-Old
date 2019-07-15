e0ee9d8eda54aef8a5e7bf5f39c071dc848a5b5e
Isabelle <isabelle.xu@wustl.edu>
2019-07-14 22:07:01 -0400

    first push

--- renderer.js
+++ renderer.js
@@ -1,3 +1,270 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var Typed = require('typed.js');
+var howler = require('howler');
+
+let bgm_link = "./src/music/bgm/";
+let se_link = "./src/music/se/"
+let curr_bgm;
+let se;
+let isTyping = false;
+let mode = "none";
+let curr_line = 0;
+
+window.onload = function() {
+    // titlescreen on start
+    
+    // play titlescreen music
+    playBgm("danse-morialta-by-kevin-macleod.mp3");
+
+    // document.getElementById("userInput").addEventListener("keyup", function(event) {
+    //     if (mode === "input") { // if not in cutscene/choice
+    //         if (event.keyCode === 13) { // enter key
+    //             if (isTyping == false) {
+    //                 // Cancel the default action, if needed
+    //                 event.preventDefault();
+    //                 let str = document.getElementById("userInput").value;
+    //                 document.getElementById("inputBox").innerHTML = "> " + str;
+    //                 getResponse(str);
+    //                 document.getElementById("userInput").value = "";
+    //             }
+    //         }
+    //     }
+    //   });
+}
+
+// retrieves story response to user input
+function getResponse(str) {
+    str.toLowerCase();
+
+    
+}
+
+// performs a typewriter effect given an string
+function typeWriter(str, spd) {
+    document.getElementById("msgBox").innerHTML = "";
+    // add pause wherever there is two spaces
+    str = str.replace(/ +(?= )/g, ' ^500');
+    //console.log(str);
+    isTyping = true;
+    var typed = new Typed("#msgBox", {
+        strings: [str],
+        typeSpeed: spd,
+        showCursor: false,
+        onComplete: function(self) {
+            isTyping = false;
+        },
+    });
+    
+}
+
+// -------- START MENU FUNCTIONS -------- 
+
+/*
+ * Starts a brand new game 
+ */
+function startGame() {
+    // fade out titlescreen
+    stopBgm();
+    fadeout("#titlescreen", 3000, function() {
+       // wait for fadeout to finish
+        getNext();
+        document.body.onkeyup = function(e){
+            if(!isTyping) {
+                 // wait for space to progress story
+                if(e.keyCode == 32){
+                    getNext();
+                }
+            } else {
+                // skipping allowed?
+            }
+        }
+    }); 
+}
+
+// goes to obj with correct label
+function find_label(labelStr) {
+
+}
+
+// -------- STORY PARSING/PATHING FUNCTIONS --------
+
+// progresses story
+function getNext() {
+    if(curr_line < story.length) { // if ending is not reached
+      let curr_step = story[curr_line]; // get the dialogue obj
+      if (undefined !== curr_step.sprite) {
+        // change sprite. If none, show nothing
+        let spriteRef = document.getElementById("sprite");
+        if(curr_step.sprite === " ") {
+            spriteRef.src = "";
+        } else {
+            spriteRef.src = "./src/img/sprites/" + curr_step.sprite;
+        }
+        curr_line++;
+
+      } 
+      if (undefined !== curr_step.bgImg) {
+        // change background image
+        let backgroundRef = document.getElementById("background");
+        if(curr_step.bgImg === "") {
+            backgroundRef.src = "";
+        } else {
+            backgroundRef.src = "./src/img/bgImg/" + curr_step.bgImg;
+    
+        }
+        curr_line++;
+      }
+      if (undefined !== curr_step.bgm) {
+          // change background music
+        playBgm(curr_step.bgm);
+        curr_line++;
+      }
+      if (undefined !== curr_step.se) {
+        // play sound effect (once)
+        playSe(curr_step.se);
+        curr_line++;
+      }
+      if (undefined !== curr_step.msg) {
+        // update name if necessary
+        if (undefined !== curr_step.name) {
+
+        }
+        // display text
+        typeWriter(curr_step.msg, DEFAULT_SPEED);
+        if (undefined !== curr_step.next) {
+          curr_line = find_label(curr_step.next); // jump to next label if not undefined
+        } else {
+          curr_line++;
+        }
+  
+      } else if (undefined !== curr_step.choice) { // if choice prompt available...
+  
+        // display the question: current_step.choice
+        // display the answers: current_step.ans
+        // choose an answer
+        // and change curr_line accordingly
+  
+      }
+    }
+  }
+
+// -------- PAUSE MENU FUNCTIONS --------
+// Pause menu button display functions
+function on() {
+    fadein("#pauseOverlay", 1000);
+}
+
+function off() {
+    fadeout("#pauseOverlay", 1000);
+}
+
+function gotoTitle() {
+    off();
+    fadein("#titlescreen", 3000);
+}
+
+// -------- FADE FUNCTIONS --------
+
+function fadeout(element, speed, callback) {
+    if(callback){
+        $(element).fadeOut(speed,callback);
+    } else {
+        $(element).fadeOut(speed);
+    }
+   
+}
+
+function fadein(element, speed, callback) {
+    if(callback){
+        $(element).fadeIn(speed,callback);
+    } else {
+        $(element).fadeIn(speed);
+    }
+
+}
+
+// -------- AUDIO FUNCTIONS --------
+
+function playBgm(bgm) {
+    // fade out last bgm (if applicable)
+    if(curr_bgm != undefined) { 
+        console.log("fading out");
+        curr_bgm.fade(0.5, 0, 2000); // fade out old bgm
+        curr_bgm.on('fade', function(){ 
+            console.log('faded bgm. Switching to next');
+            console.log("playing bgm " + bgm_link + bgm);
+            // switch bgm
+            let another_bgm = new howler.Howl({
+                src: [bgm_link + bgm],
+                autoplay: true,
+                loop: true,
+                volume: 0.5,
+                onfade: function(){
+                    curr_bgm.on('fade', function(){}); 
+                    curr_bgm = another_bgm;
+                }
+            });
+            // fade in new bgm
+            another_bgm.fade(0,0.5,3000);
+        });
+        return;
+    } else {
+        console.log("playing bgm " + bgm_link + bgm);
+        // just create new Howl and play
+        curr_bgm = new howler.Howl({
+            src: [bgm_link + bgm],
+            autoplay: true,
+            loop: true,
+            volume: 0.0
+        });
+        // fade in new bgm
+        curr_bgm.fade(0,1.0,3000);
+    }
+
+   
+}
+
+// pauses the current set bgm
+function pauseBgm() {
+    // stops the current bgm
+    curr_bgm.fade(1, 0, 1000);
+    curr_bgm.on('fade', function(){
+        curr_bgm.pause();
+    });
+}
+
+function stopBgm() {
+    curr_bgm.fade(1, 0, 2000);
+    curr_bgm.on('fade', function(){
+        curr_bgm.stop();
+    });
+}
+
+// plays inputed audio file once 
+function playSe(se) {
+    // if multiple sound effects are to be played at the same time...
+    if(se.length > 1) {
+        for (let i = 0; i < se.length; i++) {
+            console.log("Playing se: " + se_link + se);
+            let s = new howler.Howl({
+                src: [se_link + se[i]],
+                autoplay: true,
+                volume: 0.7,
+                loop: false
+            });
+        }
+    } else {
+        console.log("Playing se: " + se_link + se);
+        se = new howler.Howl({
+            src: [se_link + se[0]],
+            autoplay: true,
+            volume: 0.7,
+            loop: false
+        });
+    }
+    
+}
+
---
+++ script.js
@@ -0,0 +1,63 @@
+// characters
+const j = "Joel"; // Joel Emerson, off duty investigator for the local police 
+const s = "Sidney"; // jounalist for an urban exploration blog
+const r = "Remus"; // Remus Hawthorn, artist and owner of mansion
+
+const DEFAULT_SPEED = 30;
+
+var story = [
+    // { msg: "An endless sea stretches out before you.  There's no waves,  no ripples,  nothing."},
+    // { msg: "The sky is reflected almost perfectly onto the water,  the clouds loftily floating by."},
+    // { msg: "Your body feels heavy,  sleepy almost..."},
+    // { msg: "Soon enough,  it all just fades away...."},
+    // { msg: "............" },
+    // { msg: "........................"},
+    // { msg: "It's pitch black and silent.  You can't see or hear a thing.  Your head throbs " + 
+    // "painfully, and your ears ring with an unpleasantly high pitch."},
+    // {se: ["woodCreak.mp3"]},
+    // { msg: "Something creaks from underneath you.  A wooden chair, perhaps?  Ropes tightly bind your " +
+    // "wrists and feet to its arms and legs,  digging uncomfortably into your skin."},
+    // {msg:"........."},
+    // {se: ["ropeCreak.mp3"]},
+    // {msg:"Struggling,  you attempt to wiggle free from your bounds."},
+    // {se: ["ropeCreak.mp3"]},
+    // { msg: "What's this?  Your left hand seems to be coming loose!"},
+    // { label: "chair_struggle", event: "space", label: "Struggle", result: [
+    //     {success: "intro_success"},
+    //     {fail: "intro_fail"}
+    // ]},
+
+    // {label: "intro_fail", se: ""}, // play fail sound
+    // {msg: "You wince in pain, your wrist still bound tightly to the chair.  You inhale, " + 
+    // "preparing for your next attempt.", next: "chair_struggle"},
+    // {label: "intro_success", msg: "You manage to squeeze out your left wrist out of the rope,  " + 
+    // "your skin feeling raw and red from the effort."},
+    // {msg: "It doesn't take too long to untie your other limbs.  You stand up, stretching out " + 
+    // "your aching back.  Just how long were you tied up here?"},
+    // {se: ["train_starting.wav", "steam.mp3"]},
+    // {msg: "!!!"},
+    // {msg: "Suddenly, the ground under your feet lurches forwards,  hurling you face first onto the " +
+    // "hard ground.  The sound of creaking metal and angry hissing fills the air,  like a sleeping " + 
+    // "metallic beast was coming back to life."},
+    {bgm: "train_moving.wav"},
+    {msg: "You could recognize this churning sound.  A train?"},
+    {msg: "What were you doing on a train?"},
+    {msg: "You desperately wrack your brain, trying to remember something, anything!"},
+    {msg: "The moment something seems to flicker in your mind,  a searing hot pain shoots through your "+
+    "temples,  drowning out any possibility of a memory with excruciating static.  The pain disappears "+
+    "almost as soon as it appeared."},
+    {msg:"…It’s no good.  You just can’t remember anything without bringing on a debilitating migraine.  "+
+    "You shake your head.  How cliche."}
+
+  ];
+
+// EX.) CHOICE MENU
+//   { choice: "Do you like it?", ans: [
+//     { msg: "yes", next: "like_yes" },
+//     { msg: "no", next: "like_no" },
+//   ] },
+
+// EX.) SPRITE/BACKGROUND IMAGE CHANGE
+// { sprite: " ", bgImg: ""}, 
+
+
